7e0d1821832241c749beb6df76136ba72be96c0d
mariasmolina <mariia.smolina@gmail.com>
2025-10-22 14:30:03 +0300

    Created repository

---
+++ SortIt/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SortIt
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SortIt/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SortIt")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SortIt.Pages")]
