bcd655c0bf2c3858f83fddc5d88f742ba4ce0b95
mariasmolina <mariia.smolina@gmail.com>
2025-10-30 20:21:08 +0200

    MainPage done

---
+++ SortIt/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using SortIt.Views;
+
+namespace SortIt.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnStartClicked(object sender, EventArgs e)
+    {
+        Application.Current!.MainPage = new AppShell();
+    }
+}
