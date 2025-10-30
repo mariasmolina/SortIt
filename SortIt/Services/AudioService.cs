67cde78583f2238eca9d2a2e915ccb9d4e0348b6
mariasmolina <mariia.smolina@gmail.com>
2025-10-30 16:47:00 +0200

    AudioService and sounds

---
+++ SortIt/Services/AudioService.cs
@@ -0,0 +1,40 @@
+﻿using Plugin.Maui.Audio;
+
+namespace SortIt.Services;
+
+public sealed class AudioService
+{
+    private readonly IAudioManager audioManager = AudioManager.Current;
+    private IAudioPlayer? okSound;
+    private IAudioPlayer? errorSound;
+    private bool _isEnabled = true;
+
+    public async Task PrepareSounds()
+    {
+        okSound = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("success.mp3"));
+        errorSound = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("error.mp3"));
+        okSound.Volume = 1.0;
+        errorSound.Volume = 1.0;
+    }
+
+    public void SetEnabled(bool enabled)
+    {
+        _isEnabled = enabled;
+    }
+
+    public bool IsEnabled => _isEnabled;
+
+    public void PlayCorrect()
+    {
+        if (!_isEnabled || okSound is null) return;
+        okSound.Stop();
+        okSound.Play();
+    }
+
+    public void PlayWrong()
+    {
+        if (!_isEnabled || errorSound is null) return;
+        errorSound.Stop();
+        errorSound.Play();
+    }
+}
