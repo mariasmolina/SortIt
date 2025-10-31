31892cbee30bfca9cd821853293704639da23ea6
mariasmolina <mariia.smolina@gmail.com>
2025-10-31 13:45:13 +0200

    UserProfile Model

---
+++ SortIt/Models/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace SortIt.Models
+{
+    [Table("UserProfile")]
+    public class UserProfile
+    {
+        [PrimaryKey, AutoIncrement] public int Id { get; set; }
+        public string Name { get; set; } = "Eco Hero";
+        public string Avatar { get; set; } = "avatar_leaf.png";
+        public int Xp { get; set; } = 0;
+        public int TotalCorrect { get; set; } = 0;
+        public int TotalWrong { get; set; } = 0;
+    }
+}
